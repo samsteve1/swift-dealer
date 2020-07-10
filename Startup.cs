110e5c11505f44137997438885f548180fe22d44
Steve Idowu <idowusteve@ymail.com>
2020-07-10 16:17:31 +0100

    Configuring DBContext in Startup file

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using vega.Persistence;
 
 namespace vega
 {
@@ -18,6 +20,7 @@ namespace vega
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -25,6 +28,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkSqlServer().AddDbContext<VegaDbContext>
+            (option => option.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();
         }
 

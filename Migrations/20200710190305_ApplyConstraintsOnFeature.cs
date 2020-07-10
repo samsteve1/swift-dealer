64c5fdafb3937432ce4af373eeef402445fdfc60
Steve Idowu <idowusteve@ymail.com>
2020-07-10 20:19:41 +0100

    Building the get features endpoint

---
+++ Migrations/20200710190305_ApplyConstraintsOnFeature.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class ApplyConstraintsOnFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
